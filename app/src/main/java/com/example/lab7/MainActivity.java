7ee47e411eeba7c56dbd78f9da2c487a292cd193
akun <aliceliyukun@outlook.com>
2020-04-06 10:42:11 -0500

    -"finsihed milestone2"

--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.EditText;
 
 import static com.example.lab7.App.CHANNEL_1_ID;
 import static com.example.lab7.App.CHANNEL_2_ID;
-// finished milestone 1
+
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManagerCompat notificationManager;
@@ -36,12 +36,24 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_chat_black_24dp)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
 
         notificationManager.notify(1, notification);
